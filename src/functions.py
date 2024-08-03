e5a4dc28c63664f3eed7987170f38020f9fe8f4b
Matvey Varenitsa <subwayminder@gmail.com>
2024-08-03 23:19:18 +0300

    Hotfix

--- src/functions.py
+++ src/functions.py
@@ -7,3 +7,4 @@ async def open_profile(ap: Playwright, account: AccountDTO) -> BrowserContext:
     ads_api_response = requests.get(ADS_API_URL + '/api/v1/browser/start?user_id=' + account.get('profile_id')).json()
     browser = await ap.chromium.connect_over_cdp(ads_api_response['data']['ws']['puppeteer'], slow_mo=int(SLOW_MODE_VALUE))
     context = browser.contexts[0]
+    return context
