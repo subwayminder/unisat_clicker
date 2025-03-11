874a34907e4542a64f28f2907748bc738a78e626
subwayminder <subwayminder@gmail.com>
2025-03-12 02:45:05 +0600

    Hotfix

--- src/playwright.py
+++ src/playwright.py
@@ -365,7 +365,7 @@ async def wallet_login(unisat_page: Page, seed_phrase: List[str], password: str)
 async def skip_alert(unisat_page: Page, timeout: int):
     try:
         await unisat_page.locator('//*[@id="__next"]/div[4]/div[3]/div[1]/div[2]/div/div[4]/div[2]/div/div[3]/label/span[1]/input').first.check(timeout=timeout)
-        await unisat_page.get_by_text('//*[@id="__next"]/div[4]/div[3]/div[1]/div[2]/div/div[4]/div[2]/div/div[4]').first.click(timeout)
+        await unisat_page.locator('//*[@id="__next"]/div[4]/div[3]/div[1]/div[2]/div/div[4]/div[2]/div/div[4]').first.click(timeout)
     except:
         pass
 
@@ -419,8 +419,6 @@ async def sign_with_wallet_fractal(unisat_page: Page, context: BrowserContext, a
         pass
     await asyncio.sleep(5)
     unisat_wallet_page = context.pages[-1]
-
-    await unisat_wallet_page.locator('//*[@id="root"]/div[1]/div/div[2]/div/div[2]').click()
 
 async def unlock_wallet(unisat_wallet_page: Page, password: str):
     await unisat_wallet_page.get_by_placeholder('Password').first.fill(password, timeout=2000)
