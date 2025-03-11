02208cfa09527b8655f5cfbd2f23368ebe991546
subwayminder <subwayminder@gmail.com>
2025-03-12 02:36:05 +0600

    Fix skipping alert

--- src/playwright.py
+++ src/playwright.py
@@ -364,8 +364,8 @@ async def wallet_login(unisat_page: Page, seed_phrase: List[str], password: str)
 
 async def skip_alert(unisat_page: Page, timeout: int):
     try:
-        await unisat_page.locator('//*[@id="__next"]/div[4]/div/div[4]/div[3]/div[2]/div/div[4]/label/span[1]/input').first.check(timeout=timeout)
-        await unisat_page.locator('#__next > div.main-container.inscribe.inscribe-new.gap16.mt16 > div > div.mt-16.radius20.border-02.linear-gradient-container > div.block1 > div.mask > div > div.button.mt24').first.click(timeout)
+        await unisat_page.locator('//*[@id="__next"]/div[4]/div[3]/div[1]/div[2]/div/div[4]/div[2]/div/div[3]/label/span[1]/input').first.check(timeout=timeout)
+        await unisat_page.get_by_text('//*[@id="__next"]/div[4]/div[3]/div[1]/div[2]/div/div[4]/div[2]/div/div[4]').first.click(timeout)
     except:
         pass
 
